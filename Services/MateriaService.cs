9293db7c4c88187df37f0b90e902de8d57d9b13c
Maura <mauagui2000@gmail.com>
2023-09-25 14:30:42 -0600

    Programación de Materia Services y IMateriaService

--- Services/MateriaService.cs
+++ Services/MateriaService.cs
@@ -1,6 +1,23 @@
-﻿namespace PortalEstudiantil.AppBlazor.Services
+﻿using PortalEstudiantil.AppBlazor.Models;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace PortalEstudiantil.AppBlazor.Services
 {
-    public class MateriaService
+    public class MateriaService : IMateriaService
     {
+        //inyección de dependencias de HttpClient
+        private readonly HttpClient client;
+        public MateriaService(HttpClient httpClient)
+        {
+            client = httpClient;
+        }
+
+        public async Task<IEnumerable<Materia>> GetAll()
+        {
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            string resp = await client.GetStringAsync($"Materia");
+            return JsonSerializer.Deserialize<IEnumerable<Materia>>(resp, options);
+        }
     }
 }
